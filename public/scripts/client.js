0adaab3d652a226350b62b114a6d1711d6bd8a47
odgerey <audreymedaino@gmail.com>
2020-06-18 00:43:33 +0000

    Form Submission using JQuery works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,8 +6,8 @@
 
 $(document).ready(function() {
 // Fake data taken from initial-tweets.json
-const data = [
-  {
+  const data = [
+    {
     "user": {
       "name": "Newton",
       "avatars": "https://i.imgur.com/73hZDYK.png"
@@ -18,8 +18,8 @@ const data = [
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
     "created_at": 1461116232227
-  },
-  {
+    },
+    {
     "user": {
       "name": "Descartes",
       "avatars": "https://i.imgur.com/nlhLi3I.png",
@@ -28,13 +28,13 @@ const data = [
       "text": "Je pense , donc je suis"
     },
     "created_at": 1461113959088
-  }
-]
+   }
+  ]
 
 
 const renderTweets = function(tweets) {
   const arrayOfTweets = [];
-  const tweet_container = $('#tweet-container')
+  const tweet_container = $('#tweet-container');
   for (let tweet of tweets){
   //arrayOfTweets.push(createTweetElement(tweet))
   const x = createTweetElement(tweet);
@@ -45,6 +45,8 @@ return arrayOfTweets;
 // takes return value and appends it to the tweets container
 }
 
+
+
 const createTweetElement = function(tweet) {
   const $tweet =`
       <article>
@@ -52,7 +54,7 @@ const createTweetElement = function(tweet) {
         <h2><img src="${tweet.user.avatars}">${tweet.user.name}</h2>
         <h2 class="alias">${tweet.user.handle}</h2>
         </header>
-        
+
         <p class="tweet">${tweet.content.text}</p>
         <footer> 
           <p><span id="datetime"></span></p>
@@ -68,4 +70,29 @@ return $tweet;
 }
 
 renderTweets(data);
+
+const ajaxGet = function() {
+  $.ajax(
+    { url: "/tweets",
+      method:"GET",
+      dataType: "JSON",
+  }).then (function(response) {
+  console.log(response)
+  });
+}
+
+$('form').on('submit', (event) => {
+  event.preventDefault();
+$.ajax ({
+   type: "POST",
+   url: "/tweets",
+   data: $("form").serialize(), 
+   success: function(data){
+    alert("THIS WORKS!");
+  }
+  }).then(function(response) {
+    console.log(response);
+  })
+})
+
 });
