d17aef40126aaec2add3a548478bf5d23db9fa8f
odgerey <audreymedaino@gmail.com>
2020-06-18 23:05:52 +0000

    Responsiveness is complete, all major components work and bugs have been fixed.Code cleaned.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,130 +5,97 @@
  */
 
 $(document).ready(function() {
-// Fake data taken from initial-tweets.json
-  // const data = [
-  //   {
-  //   "user": {
-  //     "name": "Newton",
-  //     "avatars": "https://i.imgur.com/73hZDYK.png"
-  //     ,
-  //     "handle": "@SirIsaac"
-  //   },
-  //   "content": {
-  //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   },
-  //   "created_at": 1461116232227
-  //   },
-  //   {
-  //   "user": {
-  //     "name": "Descartes",
-  //     "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //     "handle": "@rd" },
-  //   "content": {
-  //     "text": "Je pense , donc je suis"
-  //   },
-  //   "created_at": 1461113959088
-  //  }
-  // ]
 
 
+//<<----------APPENDS TWEET TO THE TOP OF PAGE && RENDERS IT ------->>
 const renderTweets = function(tweets) {
-  const arrayOfTweets = [];
-  const tweet_container = $('#tweet-container');
-  for (let tweet of tweets){
-  //arrayOfTweets.push(createTweetElement(tweet))
-  const x = createTweetElement(tweet);
-  tweet_container.prepend(x);
+  const tweetContainer = $('#tweet-container');
+    for (let tweet of tweets) {
+      const twtToTop = createTweetElement(tweet);
+      tweetContainer.prepend(twtToTop);
+    }
 }
-return arrayOfTweets;
-// calls createTweetElement for each tweet
-// takes return value and appends it to the tweets container
-}
+
+//<-------SECURS TEXT FROM CROSS SCRIPT ----------->
+
 const escape =  function(str) {
   let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 }
 
-// // const alertMessage = function (error) {
 
-// }
-
-
+//<-------TWEET CREATION ELEMENTS----------------->
 const createTweetElement = function(tweet) {
-  const $tweet =`
-      <article>
+  const $tweet =
+       `<article class="new-tweet-box">
         <header>
-        <h2><img src="${tweet.user.avatars}">${escape(tweet.user.name)}</h2>
+        <h2 class="image"><img src="${tweet.user.avatars}">${escape(tweet.user.name)}</h2>
         <h2 class="alias">${escape(tweet.user.handle)}</h2>
         </header>
 
         <p class="tweet">${escape(tweet.content.text)}</p>
         <footer> 
           <p>${moment(tweet.created_at, "").fromNow()}</p>
-          <div>
+          <div id="user-buttons">
           <button type="retweet"><i class="fa fa-retweet" aria-hidden="true"></i></button>
           <button type="like"><i class="fa fa-heart" aria-hidden="true"></i></button>
           <button type="flag"><i class="fa fa-flag" aria-hidden="true"></i></button>
           </div>
         </footer>
-      </article>`
-return $tweet;
+        </article>`
+  return $tweet;
 }
 
 
 
-
-const loadTweets= function() {
-  $.ajax(
-    { url: "/tweets",
+const loadTweets = function() {
+  $.ajax ({ 
+      url: "/tweets",
       method:"GET",
       dataType: "JSON",
-  }).then (function(tweets) {
-    $('#tweet-text').empty();
-    renderTweets(tweets)
+  })
+  .then (function(tweets) {
+    $('#tweet-container').empty();
+    $('#tweet-text').val('');
+    $("#count").text(140);
+    renderTweets(tweets);
   });
 }
 
-// function validateForm() {
-//   const check = document.forms["form"]["#tweet-text"].value;
-//   if(check === "" || check === null) {
-//     alert ("Empty field!");
-//     return false;
-//   } else if (check.length > 140){
-//     alert ("Maximum characters surpassed")
-//     return false;
-//   } 
-  
-// }
-
+// <<----------- TWEET VALIDATION & REPOST -------------------------->>
 
 $('form').on('submit', (event) => {
   event.preventDefault();
-  const tweetValidation = $("#tweet-text").val()
-  if (tweetValidation === null || tweetValidation === "") {
-   
-    $("#error").text("Field is empty")
-    //alert("What")
+  const tweetValidation = $("#tweet-text").val();
+    if (tweetValidation === null || tweetValidation === "") {
+      $('#error').fadeIn().text(`*Field is empty`);  
+      setTimeout(function() {  
+         $('#error').fadeOut("Slow");  
+      }, 5000); 
     return false;
-  } else if (tweetValidation.length > 140){
     
-    $("#error").text("Maximum characters exceeded")
-    //alert("Maximum characters exceeded");
+    } else if (tweetValidation.length > 140) {
+      $("#error").fadeIn().text("*Maximum characters exceeded");  
+      setTimeout(function() {  
+         $('#error').fadeOut("Slow");  
+      }, 5000); 
     return false;
-  }
-  // } else {
-  // alert("THIS WORKS!");
-  // }
+
+    } else {
+     console.log("This tweet is posted:", tweetValidation);
+    }
 
 
 
-$.ajax ({
-   method: "POST",
-   url: "/tweets",
-   data: $("form").serialize(), 
-  }).then(function(response) {
+  $.ajax ({
+    method: "POST",
+    url: "/tweets",
+    data: $("form").serialize(), 
+    })
+  .then(function(response) {
     console.log(response);
+    $('#tweet-container').empty();
     loadTweets();
   })
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,31 +1,20 @@
 $(document).ready(function(){
-//<--------------- DOING SAME THING WITHOUT JQUERY --------------->
-  // const form = document.getElementById("tweet-text")
-  // form.addEventListener("keypress", function (event) {
-  // const currentLength = event.target.value.length
-  // })
-//<---------------- JQUERY with KEYPRESS ------------------------->
-  // $("#tweet-text").on("keypress", function () {
-  // })
+
 //<-----------------JQUERY---------------------------------------->  
 $('#tweet-text').on("input", function(){
     const maxLength = 140
     $("#count").text(maxLength)
     const count = $('#count')
     const currentLength = $(this).val().length;
-     if (currentLength > maxLength) {
+    if (currentLength > maxLength) {
        count.addClass('red')
-     } else {
-       count.removeClass('red');
-     }
-     count.text(maxLength - currentLength)
+    } else {
+      count.removeClass('red');
+    }
+      count.text(maxLength - currentLength)
 
   });  
-
-  const dt = new Date();
-document.getElementById("datetime").innerHTML = dt.toLocaleDateString();
-  
-  });
+});
 
 
 
